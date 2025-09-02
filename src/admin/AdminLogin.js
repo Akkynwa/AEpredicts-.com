b7ad8edfb16f58659ccd79704fc2a22e56d4d43e
nwali akachukwu joshua <akachukwunwali@gmail.com>
2025-09-02 23:22:46 +0930

    update

--- src/admin/AdminLogin.js
+++ src/admin/AdminLogin.js
@@ -9,7 +9,7 @@ const AdminLogin = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch(`${import.meta.env.VITE_API_URL}/admin/login`, {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/admin/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
