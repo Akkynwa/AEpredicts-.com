cea549fd1ce2187255107729df643615f15d9da4
nwali akachukwu joshua <akachukwunwali@gmail.com>
2025-09-06 01:39:34 +0930

    Trigger redeploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-require('dotenv').config();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
